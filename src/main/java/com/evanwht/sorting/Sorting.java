d8f53051f3ae0efc40a88850e66b2520eba886ee
Evan White <evanwht1@gmail.com>
2019-03-17 16:20:17 -0500

    Bubble sort

--- src/main/java/com/evanwht/sorting/Sorting.java
+++ src/main/java/com/evanwht/sorting/Sorting.java
@@ -1,5 +1,7 @@
 package com.evanwht.sorting;
 
+import java.util.Arrays;
+
 /**
  * Utility class of Searching algorithms.
  *
@@ -11,9 +13,10 @@ public class Sorting {
 	private Sorting() {}
 
 	/**
-	 * Performs a selection sort on the given array modifying it. Algorithm consists of traversing the array to find
-	 * the smallest value to the right of the current index then swapping that value with the one at the current
-	 * index. This is done for each index of the array.
+	 * Performs a selection sort on the given array modifying it.
+	 * </p>
+	 * Algorithm: Traverses the array to find the smallest value to the right of the current index then swaps that
+	 * value with the one at the current index. This is done for each index of the array.
 	 * </p>
 	 * Possibly the slowest sorting algorithm.
 	 *
@@ -33,6 +36,32 @@ public class Sorting {
 				if (min != array[i]) {
 					array[minIndex] = array[i];
 					array[i] = min;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Performs a bubble sort on the given array modifying it.
+	 * </p>
+	 * Algorithm: starting at the second index, traverse the array swapping the previous value with the current one if
+	 * the previous one is bigger than the current value. Repeat, starting at the same index but traversing until one
+	 * less than the previous ending index. Effectively "bubbles" the next maximum value to the end of the array.
+	 * </p>
+	 * Not as bad as selection but still very slow algorithm
+	 *
+	 * @param array array to sort (will modify the given array)
+	 */
+	public static void bubbleSort(final int[] array) {
+		if (array != null) {
+			for (int i = array.length; i > 0; i--) {
+				// start at index 1 so we can compare the first
+				for (int j = 1; j < i; j++) {
+					if (array[j-1] > array[j]) {
+						int swap = array[j];
+						array[j] = array[j-1];
+						array[j-1] = swap;
+					}
 				}
 			}
 		}
--- src/test/java/com/evanwht/sorting/SortingTest.java
+++ src/test/java/com/evanwht/sorting/SortingTest.java
@@ -2,6 +2,8 @@ package com.evanwht.sorting;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertArrayEquals;
 
 public class SortingTest {
@@ -10,6 +12,13 @@ public class SortingTest {
 	public void selectionSort() {
 		final int[] array = new int[] {4, 1, 8, 3, 2};
 		Sorting.selectionSort(array);
+		assertArrayEquals(new int[] {1, 2, 3, 4, 8}, array);
+	}
+
+	@Test
+	public void bubbleSort() {
+		final int[] array = new int[] {4, 1, 8, 3, 2};
+		Sorting.bubbleSort(array);
 		assertArrayEquals(new int[] {1, 2, 3, 4, 8}, array);
 	}
 }
